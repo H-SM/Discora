4370085ccaf0ef3b3a549d296186d19be87a4909
your.user.name <harmanmalht777@gmail.com>
2023-12-29 19:04:33 +0530

    feat: finished serverList Sidenav

--- client/src/components/Servers.jsx
+++ client/src/components/Servers.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Servers = () => {
   return (
-    <div className='bg-white w-[3rem] h-[3rem] rounded-2xl hover:rounded-xl transition ease-in-out duration-150'>
-      
+    <div className='flex flex-col gap-2'>
+      {/* here will be the list of the servers where the user is part of */}
+    <div className='active:bg-indigo-500 hover:bg-indigo-500 bg-[#313338] w-[3rem] h-[3rem] rounded-2xl hover:rounded-xl transition ease-in-out duration-150 flex items-center justify-center z-5'>
+      {/* {!Servers.icon ?  */}
+      <p className='text-white/90 text-[0.765rem]'>hsm-s</p>
+      {/* } */}
+    </div>
+    <div className='active:bg-indigo-500 hover:bg-indigo-500 bg-[#313338] w-[3rem] h-[3rem] rounded-2xl hover:rounded-xl transition ease-in-out duration-150 flex items-center justify-center'>
+        <img className='object-cover w-[3rem] h-[3rem] rounded-2xl hover:rounded-xl' src="https://images.unsplash.com/photo-1500964757637-c85e8a162699?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fHww" alt="server_icon"/>
+      </div>
     </div>
   )
 }
--- client/src/components/UserMessage.jsx
+++ client/src/components/UserMessage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import logo from '../assets/logo_discora_nobg_new.png'
 const UserMessage = () => {
   return (
-    <div className='bg-white w-[3rem] h-[3rem] rounded-full hover:rounded-xl transition ease-in-out duration-150'>
-      
-    </div>
+    <button className='active:bg-indigo-500 hover:bg-indigo-500 bg-[#313338]  w-[3rem] h-[3rem] rounded-full hover:rounded-xl transition ease-in-out duration-150 flex items-center justify-center'>
+      <img src={logo} alt="logo" className='w-11 h-11'/>
+      {/* this button will be linked to 1v1 chat section  */}
+    </button>
   )
 }
 
