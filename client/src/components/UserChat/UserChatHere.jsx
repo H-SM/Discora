71e20128bf006d6d47eae32b92f9f8ca59d6a85e
H-SM <harmanmalht777@gmail.com>
2024-01-08 21:01:55 +0530

    feat: user chat panel & chat description section updation

--- client/src/components/UserChat/UserChatHere.jsx
+++ client/src/components/UserChat/UserChatHere.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import UserSideNav from './UserSideNav'
 import UserFriendsMenu from './UserFriendsMenu'
 import UserRightSec from './UserRightSec'
+import UserChatSection from './UserChatSection'
 
 const UserChatHere = () => {
   return (
     <div className='w-full h-full flex flex-row justify-between'>
         <div className='h-full flex flex-row'>
         <UserSideNav/>
-        <UserFriendsMenu/>
+        {false ? <UserFriendsMenu/> : <UserChatSection/>} 
         </div> 
         <UserRightSec/>
     </div>
