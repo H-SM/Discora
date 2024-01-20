367c240232733c2074e975eb57b57f777946e687
H-SM <harmanmalht777@gmail.com>
2024-01-20 21:15:05 +0530

    feat: Server Listing components

--- client/src/components/UserChat/UsersLeftSecUser.jsx
+++ client/src/components/UserChat/UsersLeftSecUser.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FriendListUser from './FriendListUser'
+import CombinedServerList from '../ServerLists/CombinedServerList'
 
 const UsersLeftSecUser = () => {
   return (
@@ -7,12 +8,13 @@ const UsersLeftSecUser = () => {
     <div className='flex flex-col justify-start items-center gap-2 w-full overflow-y-auto overflow-x-hidden h-[86.2vh] pt-4'>
       {true ?
       <>
-        <div className='w-[31vh] h-fit flex justify-center items-center pb-1  border-white/20 border-b-[0.01rem]'>
+        <div className='w-[14rem]  h-fit flex justify-center items-center pb-1  border-white/20 border-b-[0.01rem]'>
         <div className='w-full h-fit flex gap-3 items-center focus:text-white hover:text-white active:text-white text-gray-400/80 justify-start hover:bg-white/5 active:bg-white/10 focus:bg-white/10 px-[2rem] rounded-md hover:cursor-pointer py-1 mx-2'>
         <svg viewBox="0 0 24 24" className="w-6 h-6" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill-rule="evenodd" clip-rule="evenodd" d="M7 2a1 1 0 0 0-1 1v1.001c-.961.014-1.34.129-1.721.333a2.272 2.272 0 0 0-.945.945C3.116 5.686 3 6.09 3 7.205v10.59c0 1.114.116 1.519.334 1.926.218.407.538.727.945.945.407.218.811.334 1.926.334h11.59c1.114 0 1.519-.116 1.926-.334.407-.218.727-.538.945-.945.218-.407.334-.811.334-1.926V7.205c0-1.115-.116-1.519-.334-1.926a2.272 2.272 0 0 0-.945-.945C19.34 4.13 18.961 4.015 18 4V3a1 1 0 1 0-2 0v1H8V3a1 1 0 0 0-1-1zM5 9v8.795c0 .427.019.694.049.849.012.06.017.074.049.134a.275.275 0 0 0 .124.125c.06.031.073.036.134.048.155.03.422.049.849.049h11.59c.427 0 .694-.019.849-.049a.353.353 0 0 0 .134-.049.275.275 0 0 0 .125-.124.353.353 0 0 0 .048-.134c.03-.155.049-.422.049-.849L19.004 9H5zm8.75 4a.75.75 0 0 0-.75.75v2.5c0 .414.336.75.75.75h2.5a.75.75 0 0 0 .75-.75v-2.5a.75.75 0 0 0-.75-.75h-2.5z" fill="currentColor"></path></g></svg>
             <p className='font-normal select-none font-roboto'>Events</p>
         </div>
         </div>
+        <CombinedServerList/>
       </> 
       :
       <>
