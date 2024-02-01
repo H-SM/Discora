f1c408f18537db6461b0ae26d54141f4aefdf7bb
H-SM <harmanmalht777@gmail.com>
2024-02-01 22:28:24 +0530

    feat: added a temporary login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
+import { useContext, useEffect } from "react";
 import ServerList from "../components/ServerList";
 import UserChat from "../components/UserChat/UserChat";
+import { UserContext } from "../context/user/userContext";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Home = () => {
-
+  const navigate = useNavigate();
+  const context = useContext(UserContext);
+  const { UserDetailsFirebase } = context;
+  useEffect(() => {
+    if(!UserDetailsFirebase) navigate("/login"); 
+  })
   return (
     <div className="w-[100%] h-[100vh] flex flex-row">
       <ServerList />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,66 @@
 
-import { useContext } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/user/userContext";
+import logo from "../assets/logo_discora_nobg_new.png"
 
 
 const Login = () => {
 
   const context = useContext(UserContext);
-  const { SetMyDetail } = context!;
+  const { RegisterUser } = context!;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    setState: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setState(e.target.value);
+  };
 
   return (
-    <div>
-      login here
+   <div className="w-full h-[100vh] flex justify-center items-center">             
+  <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+    <div className="px-6 py-4">
+        <div className="flex justify-center mx-auto">
+            <img className="w-[12vh]" src={logo} alt=""/>
+        </div>
+
+        <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">Welcome Back</h3>
+
+        <p className="mt-1 text-center text-gray-500 dark:text-gray-400">Login or create account</p>
+
+        <form className="text-white">
+            <div className="w-full mt-4">
+                <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" value={email} onChange={(e) => handleInputChange(e, setEmail)}/>
+            </div>
+
+            <div className="w-full mt-4">
+                <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" value={password} onChange={(e) => handleInputChange(e, setPassword)}/>
+            </div>
+
+            <div className="w-full mt-4">
+                <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" placeholder="Name" aria-label="Name" value={name} onChange={(e) => handleInputChange(e, setName)}/>
+            </div>
+
+            <div className="flex items-center justify-between mt-4">
+                <a href="#" className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500">Forget Password?</a>
+
+                <button className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                    Sign In
+                </button>
+            </div>
+        </form>
     </div>
+
+    <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+        <span className="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
+
+        <a href="#" className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Register</a>
+    </div>
+</div>  
+</div>    
   )
 }
 
